c1f4ec68d81c49d325c7ebbd6cbd41e077167f12
madhuri <thombremadhuri2001@gmail.com>
2023-12-25 16:19:35 +0530

    first commit

---
+++ src/Details.js
@@ -0,0 +1,109 @@
+// Enter all your detials in this file
+
+// Profile Image
+import madhuu from "./assets/madhuu.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/e_commerce2.png";
+import projectImage2 from "./assets/projects/portfolio.png";
+import projectImage3 from "./assets/projects/Pizza_Web.png";
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Madhuri Thombre",
+  tagline: "I Am A Front-End Developer",
+  img: madhuu,
+  about: `Hello , I am Madhuri Thombre I Have  6+ months experienced in Front End Development, React.JS Developent and little bit Experience in React Native Developent also with hands-on experience in identifying web-based user interactions along with designing and implementing highly-responsive user interface components by deploying React concepts. Proficient in translating designs and wireframes into high-quality code, and writing application interface code via JavaScript and React JS workflows. Adept at monitoring and maintaining frontend performance and troubleshooting and debugging the same to bolster overall performance`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/madhuri-thombre-1a2403226/",
+  github: "https://github.com/madhuu20",
+  // twitter: "https://twitter.com/yogesh_kendhale",
+  instagram: "https://www.instagram.com/_madhuu20_/",
+};
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "React Js Developer, Front-End Developer, React Native Developer",
+    Company: "Skillhub IT Solutions",
+    Location: "Auranagabad",
+    Duration: "March 2023 - November 2023",
+  },
+  {
+    Position: "BCS (Bachelors's of Computer Computer)",
+    Company: `Yeshwantrow Chavan College Of Art's, Commerce And Science, Sillod, Aurangabad`,
+    Univercity: "Dr. Babasaheb Ambedkar Marathwada University",
+
+    Duration: "2022 - 2025",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "React E-Commerce",
+    image: projectImage1,
+    description: `The React Planet project fetches product data, enables adding and removing items from the cart, and ensures the updated cart is saved in local storage, providing a seamless shopping experience with persistent cart data`,
+    techstack: "TailWindCss, React , Fetch API , localStorage",
+    previewLink: "http://eccomercereact.netlify.app",
+    githubLink: "https://github.com/madhuu20/planet-tamplet",
+  },
+  {
+    title: "Pizza_Web",
+    image: projectImage3,
+    description: `In This Project I created A template using Tailwind Css and using Javascript Filter method i filter Pizza category and price .`,
+    techstack: "HTML/Tailwind css, JavaScript , ReactJs",
+    previewLink: "https://pizza-web-lyart.vercel.app/",
+    githubLink: "https://github.com/madhuu20/planet-tamplet",
+  },
+  // {
+  //   title: "Pesonal Portfolio",
+  //   image: projectImage2,
+  //   description: `I utilized a GitHub template to develop a customized React portfolio, incorporating my preferred modifications. The final version was then hosted on Netlify, enabling easy accessibility and showcasing my work to potential visitors.`,
+  //   techstack: "React, Tailwind , Javascript , Css, Github",
+  //   previewLink: "https://personal-portfoli-three.vercel.app/",
+  //   githubLink: "https://github.com/yogi-18/personal_portfolio",
+  // },
+
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "thombremadhuri2001@gmail.com",
+  phone: "+91 9359861965",
+};
---
+++ src/Pages/About.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Work from "../Components/Work";
+import { personalDetails,  eduDetails } from "../Details";
+
+function About() {
+  return (
+    <main className="container mx-auto max-width pt-10 pb-20 ">
+      <section>
+        <h1 className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+          About Me
+        </h1>
+        <p className="text-content py-8 lg:max-w-3xl">{personalDetails.about}</p>
+      </section>
+  
+      <section>
+        <h1 className="text-2xl pt-10 text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+          Education
+        </h1>
+        {React.Children.toArray(
+          eduDetails.map(({ Position, Company, Location, Duration }) => (
+            <Work
+              position={Position}
+              company={Company}
+              location={Location}
+              duration={Duration}
+            />
+          ))
+        )}
+      </section>
+    </main>
+  );
+}
+
+export default About;
